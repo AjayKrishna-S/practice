6517073d4b9a82307ab318efc03a5410dc729fcd
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-05-26 20:24:08 +0530

    print thr grid

--- JAVA/Sort.java
+++ JAVA/Sort.java
@@ -28,5 +28,17 @@ public class Sort {
         Arrays.sort(my_array2);
 
         System.out.println("sort : " + Arrays.toString(my_array2));
+
+        for(int i = 0; i <= 10; i++)
+        {
+            for(int j = 0; j <= 10; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
+
+
+    
 }
